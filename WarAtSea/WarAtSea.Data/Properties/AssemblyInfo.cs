35f57df51cdf999d6c04276feb4d9c8307af8d07
WookieSlapper <dominicmazella@gmail.com>
2016-03-13 14:43:03 -0400

    Got caught up in upgrading to MvC 5.... there's some setback

--- WarAtSea/WarAtSea.Data/Properties/AssemblyInfo.cs
+++ WarAtSea/WarAtSea.Data/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WarAtSea.Tests")]
+[assembly: AssemblyTitle("WarAtSea.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WarAtSea.Tests")]
+[assembly: AssemblyProduct("WarAtSea.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0529784d-e085-4dd0-9648-1605a5eef025")]
+[assembly: Guid("a951685d-cd39-40a9-9dab-1c4659830896")]
